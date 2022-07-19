253b6e72f041de581d7394cce5e9fc6a3c71f5f0
goats-9 <goats.singh@gmail.com>
2022-07-19 06:04:39 +0530

    Updated template

--- template.cpp
+++ template.cpp
@@ -11,6 +11,8 @@ typedef vector<long long> vll;
 typedef map<int, int> mii;
 #define f(i, a, b, j) for(int i = int(a); i < int(b); i = i + int(j))
 #define pb push_back
+#define reset(a, b) memset(a, int(b), sizeof(a))
+#define MOD 1e9 + 7
 
 int main() {
 	int t;
