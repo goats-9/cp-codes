5e88b93f24c6827920c2b21af9c25c412976da52
goats-9 <goats.singh@gmail.com>
2022-08-06 22:04:26 +0530

    Update template

--- template.cpp
+++ template.cpp
@@ -9,10 +9,16 @@ typedef pair<long long, long long> pll;
 typedef vector<int> vi;
 typedef vector<long long> vll;
 typedef map<int, int> mii;
+typedef vector<vector<int>> vvi;
+typedef vector<vector<long long>> vvll;
 #define f(i, a, b, j) for(int i = int(a); i < int(b); i = i + int(j))
+#define M map
+#define V vector
+#define nl "\n"
 #define pb push_back
+#define mp make_pair
 #define reset(a, b) memset(a, int(b), sizeof(a))
-#define MOD 1e9 + 7
+#define MOD (int)1e9 + 7
 
 int main() {
 	int t;
