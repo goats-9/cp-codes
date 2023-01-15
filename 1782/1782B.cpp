9f9ab5cefb59f7535998846ed5c22d4606716234
Gautam Singh <goats.singh@gmail.com>
2023-01-15 20:33:56 +0530

    talent issue

---
+++ 1782/1782B.cpp
@@ -0,0 +1,43 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+typedef unsigned long long ull; 
+typedef pair<int, int> pii;
+typedef pair<double, double> pdd;
+typedef pair<long long, long long> pll;
+typedef vector<int> vi;
+typedef vector<long long> vll;
+typedef map<int, int> mii;
+typedef vector<vector<int>> vvi;
+typedef vector<vector<long long>> vvll;
+#define f(i, a, b, j) for(int i = int(a); i < int(b); i = i + int(j))
+#define M map
+#define V vector
+#define P pair
+#define all(a) a.begin(), a.end() 
+#define rall(a) a.rbegin(), a.rend() 
+#define nl "\n"
+#define pb push_back
+#define mp make_pair
+#define reset(a, b) memset(a, int(b), sizeof(a))
+#define MOD (int)1e9 + 7
+
+int main() {
+	int t, n;
+    cin >> t;
+    while (t--) { 
+        cin >> n;
+        vi a(n);
+        f(i, 0, n, 1) cin >> a[i];
+        sort(all(a));
+        int ctr = 1;
+        f(i, 0, n, 1) { 
+            if (i > 0 && a[i - 1] >= i) continue;
+            if (a[i] <= i) continue;
+            ++ctr;
+        }
+        cout << ctr << nl;
+	}
+	return 0;
+}
