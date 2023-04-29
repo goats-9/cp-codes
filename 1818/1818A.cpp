7aa6376cd7e3093524ad1a137d34dd51020ff3b5
Gautam Singh <goats.singh@gmail.com>
2023-04-29 22:21:11 +0530

    Chill the fuck out

---
+++ 1818/1818A.cpp
@@ -0,0 +1,38 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+typedef unsigned long long ull; 
+typedef pair<int, int> pii;
+typedef pair<double, double> pdd;
+typedef pair<long long, long long> pll;
+typedef vector<int> vi;
+typedef vector<long long> vll;
+typedef map<int, int> mii;
+typedef vector<vector<int>> vvi;
+typedef vector<vector<long long>> vvll;
+#define f(i, a, b, j) for(int i = int(a); i < int(b); i = i + int(j))
+#define M map
+#define V vector
+#define P pair
+#define all(a) a.begin(), a.end() 
+#define rall(a) a.rbegin(), a.rend() 
+#define nl "\n"
+#define pb push_back
+#define mp make_pair
+#define reset(a, b) memset(a, int(b), sizeof(a))
+#define MOD (int)1e9 + 7
+
+int main() {
+	int t, n, k;
+	cin >> t;
+    while (t--) { 
+        cin >> n >> k;
+        V<string> op(n);
+        f(i,0,n,1) cin >> op[i];
+        int ctr = 0;
+        f(i,1,n,1) if (op[i] == op[0]) ++ctr;
+        cout << ctr + 1 << nl;
+	}
+	return 0;
+}
